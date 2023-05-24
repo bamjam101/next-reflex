6a300e99f184912114611fb1d71ae93b6fa07c28
bamjam101 <herejamil123@gmail.com>
2023-05-24 22:08:03 +0530

    Fetching prompts route configured.

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex justify-center items-center flex-col">
-      <h1 className="text-center font-extrabold my-2">
+      <h1 className="text-center flex flex-col font-extrabold my-2">
         Discover & Share
         <br className="max-md:hidden" />
         <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent text-center">
@@ -11,10 +11,8 @@ const Home = () => {
         </span>
       </h1>
       <p className="text-center">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
-        temporibus harum in repellendus totam aliquid eum. Assumenda cupiditate
-        accusamus dolorum porro recusandae distinctio nihil eveniet deserunt
-        aut! Quisquam, natus dolorem.
+        Find and share all the trendy prompts and redirects out there. Help the
+        community grow with your support and exploration of the AI world.
       </p>
 
       <Feed />
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 space-y-6 py-8 sm:columns-2 sm:gap-6 xl:columns-3">
+      {data?.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        ></PromptCard>
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="mt-16 mx-auto w-full max-w-xl flex justify-center items-center flex-col gap-2">
+      <form className="relative w-full flex justify-center items-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="block w-full rounded-md border border-gray-200 bg-white py-2.5 font-satoshi pl-5 pr-12 text-sm shadow-lg font-medium focus:border-black focus:outline-none focus:ring-0"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}}></PromptCardList>
+    </section>
+  );
 };
 
 export default Feed;
